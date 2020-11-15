61b4ef32e52398b444436f4db69f88233bc69302
Brandon Metz <bcmetz@jpl.nasa.gov>
2020-11-14 21:54:21 -0800

    bcmetz: setting up repos

--- TeaNC.ino
+++ TeaNC.ino
@@ -1,25 +1,6 @@
 
-
-#define USER_LED_PIN 5
-#define USER_PUSH_BTN1_PIN 39
-#define USER_PUSH_BTN2_PIN 34
-
-#define POWER_SENSE_PIN 4
-
-#define DISPLAY_ENABLE_PIN 19
-#define DISPLAY_I2C_SCL_PIN 22
-#define DISPLAY_I2C_SDA_PIN 21
-
-#define GPS_UART_RX_PIN 27
-#define GPS_UART_TX_PIN 26
-#define GPS_PPS_PIN 35
-
-#define VHF_UART_RX_PIN 33
-#define VHF_UART_TX_PIN 32
-#define VHF_PWR_DOWN_PIN 13
-#define VHF_RX_TXN_PIN 14
-#define VHF_AOUT_PIN 25
-#define VHF_AIN_PIN 36
+#include "src/config.h"
+#include "src/sa818/sa818v.h"
 
 #define VHF_TX_STATE LOW
 #define VHF_RX_STATE HIGH
